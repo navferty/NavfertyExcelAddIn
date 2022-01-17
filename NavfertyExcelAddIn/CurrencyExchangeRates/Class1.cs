4d5259b60ec10ff79e715c3097bb34fa3439b0f9
uom <uom.2000@gmail.com>
2022-01-17 22:50:38 +0700

    Added CBR web service for CurrencyExchangeRates

---
+++ NavfertyExcelAddIn/CurrencyExchangeRates/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavfertyExcelAddIn.CurrencyExchangeRates
+{
+	internal class GetCBR2
+	{
+		void DDD()
+		{
+			using (var cbr = new Web.CBR.DailyInfoSoapClient())
+			{
+				var curses = cbr.GetCursOnDate(DateTime.Now);
+			};
+		}
+
+	}
+}
