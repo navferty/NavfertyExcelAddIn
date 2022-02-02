fed242f63524a739d7892f1b793ad74c00e56d6e
uom <uom.2000@gmail.com>
2022-02-03 00:54:35 +0700

    Begin ECB support

--- NavfertyExcelAddIn/Web/CurrencyExchangeRates/DataProviders/CBRF/CBRFProvider.cs
+++ NavfertyExcelAddIn/Web/CurrencyExchangeRates/DataProviders/CBRF/CBRFProvider.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Data;
+using System.Diagnostics;
+using System.Dynamic;
 using System.Globalization;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 using NavfertyExcelAddIn.Commons;
 using NavfertyExcelAddIn.Localization;
@@ -20,6 +24,8 @@ namespace NavfertyExcelAddIn.Web.CurrencyExchangeRates.Providers
 		private DataTable rawDataTable = null;
 		protected override async Task<WebResultRow[]> DownloadWebResultRowsForDate(DateTime dt)
 		{
+			await TestECB(dt);
+
 			using (var cbr = new Web.CBR.DailyInfoSoapClient())
 			{
 				var dtsResult = await cbr.GetCursOnDateAsync(dt);
@@ -30,6 +36,38 @@ namespace NavfertyExcelAddIn.Web.CurrencyExchangeRates.Providers
 				var rows = rawDataTable.RowsAsEnumerable().Select(row => new WebResultRow(row, dt)).ToArray();
 				return rows;
 			};
+		}
+
+		private async Task TestECB(DateTime dt)
+		{
+
+			//https://sdw-wsrest.ecb.europa.eu/service/data/EXR?startPeriod=2022-02-01&endPeriod=2022-02-01
+
+			string sDate = dt.ToString("yyyy-MM-dd");
+			var urlECBExchangeForDate = @$"https://sdw-wsrest.ecb.europa.eu/service/data/EXR?startPeriod={sDate}&endPeriod={sDate}";
+			Debug.WriteLine(urlECBExchangeForDate);
+
+			using (var htc = new HttpClient())
+			{
+				var rawECBString = await (await htc.GetAsync(urlECBExchangeForDate)).
+					EnsureSuccessStatusCode().
+					Content.ReadAsStringAsync();
+
+				if (string.IsNullOrWhiteSpace(rawECBString))
+					return;
+
+				// Load an XML document.
+
+				var xr = new System.IO.StringReader(rawECBString);
+				var xDoc = XDocument.Load(xr);
+
+				dynamic root = new ExpandoObject();
+				NavfertyExcelAddIn.Web.CurrencyExchangeRates.DataProviders.CBRF.XmlDeserializerToObject.Parse(root, xDoc.Root);
+
+
+				int UUU = 9;
+				//return rawECBString;
+			}
 		}
 	}
 }
