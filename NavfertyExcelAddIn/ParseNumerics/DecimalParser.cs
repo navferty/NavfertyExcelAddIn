623f1ef91664941c46df10279021d637a97343fb
uom <uom.2000@gmail.com>
2022-02-06 19:03:01 +0700

    fixed RemoveSpacesEx lost after merging

--- NavfertyExcelAddIn/ParseNumerics/DecimalParser.cs
+++ NavfertyExcelAddIn/ParseNumerics/DecimalParser.cs
@@ -1,132 +1,129 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 
-using NavfertyExcelAddIn.Commons;
+using Navferty.Common;
 
 namespace NavfertyExcelAddIn.ParseNumerics
 {
-    /// <summary>
-    /// Parser implemented in https://github.com/navferty/NumericParser
-    /// </summary>
-    public static class DecimalParser
-    {
-        private static readonly Regex SpacesPattern = new(@"\s");
-        private static readonly Regex DecimalPattern = new(@"[\d\.\,\s]+");
-        private static readonly Regex ExponentPattern = new(@"[-+]?\d*\.?\d+[eE][-+]?\d+");
+	/// <summary>
+	/// Parser implemented in https://github.com/navferty/NumericParser
+	/// </summary>
+	public static class DecimalParser
+	{
+		private static readonly Regex SpacesPattern = new(@"\s");
+		private static readonly Regex DecimalPattern = new(@"[\d\.\,\s]+");
+		private static readonly Regex ExponentPattern = new(@"[-+]?\d*\.?\d+[eE][-+]?\d+");
 
-        public static NumericParseResult? ParseDecimal(this string value)
-        {
-            if (string.IsNullOrWhiteSpace(value))
-            {
-                return null;
-            }
+		public static NumericParseResult? ParseDecimal(this string value)
+		{
+			if (string.IsNullOrWhiteSpace(value))
+			{
+				return null;
+			}
 
-            var v = value.RemoveSpacesEx();
+			var v = value.RemoveSpacesEx();
 
-            if (ExponentPattern.IsMatch(v))
-            {
-                return new NumericParseResult(v.TryParseExponent());
-            }
+			if (ExponentPattern.IsMatch(v))
+			{
+				return new NumericParseResult(v.TryParseExponent());
+			}
 
-            if (!DecimalPattern.IsMatch(value))
-            {
-                return null;//Doesn't look like a number at all.
-            }
+			if (!DecimalPattern.IsMatch(value))
+			{
+				return null;//Doesn't look like a number at all.
+			}
 
-            //Determine the decimal separator . or ,
-            if (v.Contains(",") && v.Contains("."))
-            {
-                var last = v.LastIndexOfAny(new[] { ',', '.' });
-                var c = v[last];
-                return v.CountChars(c) == 1
-                    ? v.TryParse(c == '.' ? Format.Dot : Format.Comma)
-                    : null;
-            }
+			//Determine the decimal separator . or ,
+			if (v.Contains(",") && v.Contains("."))
+			{
+				var last = v.LastIndexOfAny(new[] { ',', '.' });
+				var c = v[last];
+				return v.CountChars(c) == 1
+					? v.TryParse(c == '.' ? Format.Dot : Format.Comma)
+					: null;
+			}
 
-            if (v.Contains(","))
-            {
-                return v.CountChars(',') == 1
-                    ? v.TryParse(Format.Comma)
-                    : v.TryParse(Format.Dot);
-            }
+			if (v.Contains(","))
+			{
+				return v.CountChars(',') == 1
+					? v.TryParse(Format.Comma)
+					: v.TryParse(Format.Dot);
+			}
 
-            if (v.Contains("."))
-            {
-                return v.CountChars('.') == 1
-                    ? v.TryParse(Format.Dot)
-                    : v.TryParse(Format.Comma);
-            }
+			if (v.Contains("."))
+			{
+				return v.CountChars('.') == 1
+					? v.TryParse(Format.Dot)
+					: v.TryParse(Format.Comma);
+			}
 
-            return v.TryParse(Format.Dot);
-        }
+			return v.TryParse(Format.Dot);
+		}
 
-        private static double? TryParseExponent(this string value)
-        {
-            return double.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture, out double result)
-                ? result
-                : null;
-        }
+		private static double? TryParseExponent(this string value)
+		{
+			return double.TryParse(value, NumberStyles.Float, CultureInfo.InvariantCulture, out double result)
+				? result
+				: null;
+		}
 
-        private static Lazy<string[]> _allCurrencySymbolsCacheLazy = new(()
-            => CultureInfo.GetCultures(CultureTypes.AllCultures)
-        .Select(ci => ci.NumberFormat.CurrencySymbol)
-        .Distinct()
-        .Where(cur => !string.IsNullOrWhiteSpace(cur))
-        .ToArray());
+		private static Lazy<string[]> _allCurrencySymbolsCacheLazy = new(()
+			=> CultureInfo.GetCultures(CultureTypes.AllCultures)
+		.Select(ci => ci.NumberFormat.CurrencySymbol)
+		.Distinct()
+		.Where(cur => !string.IsNullOrWhiteSpace(cur))
+		.ToArray());
 
-        private static NumericParseResult? TryParse(this string value, Format info)
-        {
-            var formatInfo = (NumberFormatInfo)NumberFormatInfo.InvariantInfo.Clone();
+		private static NumericParseResult? TryParse(this string value, Format info)
+		{
+			var formatInfo = (NumberFormatInfo)NumberFormatInfo.InvariantInfo.Clone();
 
-            if (info == Format.Comma)
-            {
-                formatInfo.CurrencyDecimalSeparator = ",";
-                formatInfo.CurrencyGroupSeparator = ".";
-                formatInfo.NumberDecimalSeparator = ",";
-                formatInfo.NumberGroupSeparator = ".";
-            }
-            else
-            {
-                formatInfo.CurrencyDecimalSeparator = ".";
-                formatInfo.CurrencyGroupSeparator = ",";
-            }
-            // добавить тест-кейсов на формат валют
-            //formatInfo.CurrencyNegativePattern = 8;
-            //formatInfo.CurrencyPositivePattern = 3;
+			if (info == Format.Comma)
+			{
+				formatInfo.CurrencyDecimalSeparator = ",";
+				formatInfo.CurrencyGroupSeparator = ".";
+				formatInfo.NumberDecimalSeparator = ",";
+				formatInfo.NumberGroupSeparator = ".";
+			}
+			else
+			{
+				formatInfo.CurrencyDecimalSeparator = ".";
+				formatInfo.CurrencyGroupSeparator = ",";
+			}
+			// добавить тест-кейсов на формат валют
+			//formatInfo.CurrencyNegativePattern = 8;
+			//formatInfo.CurrencyPositivePattern = 3;
 
-            var valueParsed = double.TryParse(value, NumberStyles.Currency, formatInfo, out double result);
-            if (valueParsed) return new NumericParseResult(result);//Parsed without our help
+			var valueParsed = double.TryParse(value, NumberStyles.Currency, formatInfo, out double result);
+			if (valueParsed) return new NumericParseResult(result);//Parsed without our help
 
-            //.TryParse cannot parse a string with a CurrencySymbol and number format not from the current user culture
-            //Therefore, we help to parse the string with an arbitrary currency manualy...
+			//.TryParse cannot parse a string with a CurrencySymbol and number format not from the current user culture
+			//Therefore, we help to parse the string with an arbitrary currency manualy...
 
-            //detect how many different currency symbols contains source string.
-            var currenciesInValue = _allCurrencySymbolsCacheLazy.Value.Where(cur => value.Contains(cur));//.ToArray();
-            if (currenciesInValue.Count() == 1)// TODO: If the string contains several different currency symbols, do not convert, because currency priority is not clear
-            {
-                var curSymb = currenciesInValue.First();
-                //Remove found currencySymbol from source string
-                var valueWithoutCurrencySymbol = value.Replace(curSymb, string.Empty);
-                valueParsed = double.TryParse(valueWithoutCurrencySymbol, NumberStyles.Currency, formatInfo, out result);
-                if (valueParsed)
-                {
-                    //Debug.WriteLine($"Parsed value: '{value}, valueWithoutCurrencySymbol: {valueWithoutCurrencySymbol}', result: {result}, currency: {curSymb}");
-                    return new NumericParseResult(result, curSymb);
-                }
-                //It was not possible to parse the line, even after removing the currencySymbol, most likely this is not about money at all...
-            }
-            return null;//Not found any currency symbols, or found more than one, or even not number...
-        }
+			//detect how many different currency symbols contains source string.
+			var currenciesInValue = _allCurrencySymbolsCacheLazy.Value.Where(cur => value.Contains(cur));//.ToArray();
+			if (currenciesInValue.Count() == 1)// TODO: If the string contains several different currency symbols, do not convert, because currency priority is not clear
+			{
+				var curSymb = currenciesInValue.First();
+				//Remove found currencySymbol from source string
+				var valueWithoutCurrencySymbol = value.Replace(curSymb, string.Empty);
+				valueParsed = double.TryParse(valueWithoutCurrencySymbol, NumberStyles.Currency, formatInfo, out result);
+				if (valueParsed)
+				{
+					//Debug.WriteLine($"Parsed value: '{value}, valueWithoutCurrencySymbol: {valueWithoutCurrencySymbol}', result: {result}, currency: {curSymb}");
+					return new NumericParseResult(result, curSymb);
+				}
+				//It was not possible to parse the line, even after removing the currencySymbol, most likely this is not about money at all...
+			}
+			return null;//Not found any currency symbols, or found more than one, or even not number...
+		}
 
-        private enum Format
-        {
-            Dot,
-            Comma
-        }
-    }
+		private enum Format
+		{
+			Dot,
+			Comma
+		}
+	}
 }
