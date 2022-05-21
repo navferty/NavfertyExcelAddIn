6b9214ad919734999ebf42b4ef492560ba6b1f3c
uom42 <uom.2000@gmail.com>
2022-05-21 15:12:22 +0700

    LimitTextLength

---
+++ NavfertyExcelAddIn/LimitTextLength/TextTrimmer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavfertyExcelAddIn.LimitTextLength
+{
+	internal static class TextTrimmer
+	{
+		public static string? LimitLength(this string value, int len, bool trimSpacedStrings)
+		{
+			if (string.IsNullOrEmpty(value)) return string.Empty;
+			if (trimSpacedStrings && string.IsNullOrWhiteSpace(value)) return string.Empty;
+
+			return (value.Length <= len)
+				? value
+				: value.Substring(0, len);
+		}
+
+	}
+}
